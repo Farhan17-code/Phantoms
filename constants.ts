a62b7eb9a59fd5e566fccd040b751cdf21248b2a
Farhan17-code <farhanislam11y@gmail.com>
2026-01-08 22:34:03 +0600

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -5,7 +5,7 @@ export const PRODUCTS: Product[] = [
   {
                 id: 'p1',
                 name: 'Jack Overcoat',
-                price: '19,400',
+                price: 19400,
                 description: 'Zibellino Cashmere',
                 image: 'https://ogier1948.com/cdn/shop/files/DSC03233.jpg?crop=center&height=800&v=1761814965&width=800',
                 category: 'Phantom Elite'
@@ -13,7 +13,7 @@ export const PRODUCTS: Product[] = [
             {
                 id: 'p2',
                 name: 'Kayak Fleece',
-                price: '4,300',
+                price: 4300,
                 description: 'Silk, Wool',
                 image: 'https://ogier1948.com/cdn/shop/files/og-kayak-black-01.jpg?crop=center&height=800&v=1754648781&width=800',
                 category: 'Apparel'
@@ -21,7 +21,7 @@ export const PRODUCTS: Product[] = [
             {
                 id: 'p3',
                 name: 'Naxos Windstopper',
-                price: '3,400',
+                price: 3400,
                 description: 'Wool, Microfiber',
                 image: 'https://ogier1948.com/cdn/shop/files/og-naxos-jacket-beige-01.jpg?crop=center&height=800&v=1754649870&width=800',
                 category: 'Phantom Elite'
@@ -29,7 +29,7 @@ export const PRODUCTS: Product[] = [
             {
                 id: 'p4',
                 name: 'Palm Wool Vest',
-                price: '4,200',
+                price: 4200,
                 description: 'Superfine Wool',
                 image: 'https://ogier1948.com/cdn/shop/files/og-palm-vest-beige-01.jpg?crop=center&height=800&v=1754650134&width=800',
                 category: 'Apparel'
@@ -37,7 +37,7 @@ export const PRODUCTS: Product[] = [
             {
                 id: 'p5',
                 name: 'Otto Jacket',
-                price: '4,400',
+                price: 4400,
                 description: 'Silk, Wool, Technical fabric',
                 image: 'https://ogier1948.com/cdn/shop/files/DSC04045.jpg?crop=center&height=800&v=1761831877&width=800',
                 category: 'Phantom Elite'
@@ -45,7 +45,7 @@ export const PRODUCTS: Product[] = [
             {
                 id: 'p6',
                 name: 'Figari Bomber',
-                price: '6,000',
+                price: 6000,
                 description: 'Goatskin',
                 image: 'https://ogier1948.com/cdn/shop/files/og-figari-navy-01.jpg?crop=center&height=800&v=1754648013&width=800',
                 category: 'Apparel'
@@ -53,7 +53,7 @@ export const PRODUCTS: Product[] = [
             {
                 id: 'p7',
                 name: 'Dior Oblique Hooded Sweatshirt',
-                price: '2,550',
+                price: 2550,
                 description: 'Beige Cotton-Blend Jersey',
                 image: 'https://assets.christiandior.com/is/image/diorprod/LOOK_H_25_1_LOOK_041_E07?$lookDefault_GH-GHC$&crop=568,0,1864,2000&wid=1024&hei=1107&scale=0.5535&bfc=on&qlt=85',
                 category: 'Apparel'
@@ -61,7 +61,7 @@ export const PRODUCTS: Product[] = [
             {
                 id: 'p8',
                 name: 'Dior Oblique Hooded Sweatshirt',
-                price: '2,550',
+                price: 2550,
                 description: 'Black Cotton-Blend Jersey',
                 image: 'https://assets.christiandior.com/is/image/diorprod/513J626A0978C900_E01-4?$default_GHC$&crop=502,152,996,1471&wid=1024&hei=1107&scale=0.4843&bfc=on&qlt=85',
                 category: 'Apparel'
@@ -69,7 +69,7 @@ export const PRODUCTS: Product[] = [
             {
                 id: 'p9',
                 name: 'Dior Oblique Hooded Sweatshirt',
-                price: '2,550',
+                price: 2550,
                 description: 'Navy Blue Cotton-Blend Jersey',
                 image: 'https://assets.christiandior.com/is/image/diorprod/513J626A0978C540_E01?$default_GHC$&crop=487,149,1021,1479&wid=1024&hei=1107&scale=0.4843&bfc=on&qlt=85',
                 category: 'Phantom Elite'
@@ -79,7 +79,7 @@ export const PRODUCTS: Product[] = [
             {
                 id: 'p10',
                 name: 'Dior Ribbon Overshirt',
-                price: '3,000',
+                price: 3000,
                 description: 'Blue Cotton Twill',
                 image: 'https://assets.christiandior.com/is/image/diorprod/613D497A3037C585_E01?$default_GHC$&crop=391,156,1210,1610&wid=1024&hei=1107&scale=0.4843&bfc=on&qlt=85',
                 category: 'Phantom Elite'
@@ -87,7 +87,7 @@ export const PRODUCTS: Product[] = [
             {
                 id: 'p11',
                 name: 'Cannage Overshirt with Christian Dior Couture Embroidery',
-                price: '3,300',
+                price: 3300,
                 description: 'Gray Cotton Flannel',
                 image: 'https://assets.christiandior.com/is/image/diorprod/313C509F3088C978_E01-4?$default_GHC$&crop=453,150,1095,1458&wid=1536&hei=1661&scale=0.7265&bfc=on&qlt=85',
                 category: 'Apparel'
@@ -95,7 +95,7 @@ export const PRODUCTS: Product[] = [
             {
                 id: 'p12',
                 name: 'Dior Ribbon T-Shirt, Regular Fit',
-                price: '1,000',
+                price: 1000,
                 description: 'Deep Gray Cotton Jersey',
                 image: 'https://assets.christiandior.com/is/image/diorprod/613J668A0677C889_E01?$default_GHC$&crop=455,150,1090,1350&wid=1024&hei=1107&scale=0.4843&bfc=on&qlt=85',
                 category: 'Apparel'
